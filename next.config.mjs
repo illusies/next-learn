0c06014863c9dfe840da4494230eabf5ac14a58b
Asheka Baker <ashleybk876@gmail.com>
2024-06-24 23:18:25 -0500

    reached chapter 11 in tutorial

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+/* /** @type {import('next').nextConfig}
+ 
+const nextConfig = {
+  experimental: {
+    ppr: 'incremental',
+  },
+};
+ 
 export default nextConfig;
+ */
